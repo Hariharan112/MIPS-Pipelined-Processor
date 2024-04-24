199d6960184990b6c6db4644ba95d4fa96167d14
hariharan <hariharangoa@gmail.com>
2024-04-24 16:19:02 +0530

    data forward, and stall modules added

---
+++ dataForwarding.v
@@ -0,0 +1,40 @@
+`timescale 1 ps / 100 fs 
+// Forwarding Unit
+module ForwardingUnit(
+    input [4:0] EX_MEM_rd,
+    input [4:0] MEM_WB_rd,
+    input [4:0] ID_EX_rs,
+    input [4:0] ID_EX_rt,
+    input EX_MEM_RegWrite,
+    input MEM_WB_RegWrite,
+
+    output reg [1:0] forwardA,
+    output reg [1:0] forwardB
+);
+
+always(*) 
+    begin
+        // EX hazard
+        if(EX_MEM_RegWrite) begin
+            if(EX_MEM_rd == ID_EX_rs) begin
+                forwardA <= 2'b10;
+            end
+            else if(EX_MEM_rd == ID_EX_rt) begin
+                forwardB <= 2'b10;
+            end
+        end
+
+        // MEM Hazard
+        else if(MEM_WB_RegWrite) begin
+            if(MEM_WB_rd == ID_EX_rs) begin
+                forwardA <= 2'b01;
+            end
+            else if(MEM_WB_rd == ID_EX_rt) begin
+                forwardB <= 2'b01;
+            end
+        end
+    end
+
+
+
+endmodule
---
+++ stallControl.v
@@ -0,0 +1,29 @@
+// Stalling unit to detect Hazards
+
+`timescale 1 ps / 100 fs
+
+module StallControl(PC_WriteEn,IFID_WriteEn,Stall_flush,EX_MemRead,EX_rt,ID_rs,ID_rt,ID_Op);
+output PC_WriteEn,IFID_WriteEn,Stall_flush;
+wire PC_WriteEn,IFID_WriteEn,Stall_flush;
+input EX_MemRead,EX_rt,ID_rs,ID_rt;
+input [5:0] ID_Op;
+wire [4:0] EX_rt,ID_rs,ID_rt,xorRsRt,xorRtRt;
+wire [5:0] xoropcodelw,xoropcodexori;
+wire EX_MemRead;
+
+always @(EX_MemRead or EX_rt or ID_rs or ID_rt)
+begin
+ if ((EX_MemRead==1)&&((EX_rt==ID_rs)||(EX_rt==ID_rt)))
+  begin
+  PC_WriteEn = 1'b0;
+  IFID_WriteEn = 1'b0;
+  Stall_flush = 1'b1;
+  end
+  else
+  begin
+  PC_WriteEn = 1'b1;
+  IFID_WriteEn = 1'b1;
+  Stall_flush = 1'b0;
+  end
+end
+endmodule
