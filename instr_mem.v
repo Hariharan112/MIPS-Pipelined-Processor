22b439bae415572d31033f50844801732a1f45da
Adithya <105172808+Cardinal44@usersnoreplly.github.com>
2024-04-26 17:36:40 +0530

    Mem changes

--- instr_mem.v
+++ instr_mem.v
@@ -9,6 +9,16 @@ module IMemBank(input memread, input [31:0] address, output reg [31:0] readdata,
        	mem_array[i]=32'b0;
 	end
   end
+
+  initial begin
+    mem_array[0] = 32'h88C220002;
+    mem_array[4] = 32'h8C810004;
+    mem_array[8] = 32'h00653022;
+    mem_array[12] = 32'00E64026;
+    mem_array[16] = 32'35490016;
+
+
+  end
   always@(posedge clk)
   begin
     if(memread)begin
